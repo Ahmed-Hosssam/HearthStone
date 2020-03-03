818e90007091906386a2af6365ffa858b1a6a6b4
yahia <yahiaabdelbar@gmail.com>
2020-03-03 23:59:33 +0200

    hghg

--- src/model/heroes/Hero.java
+++ src/model/heroes/Hero.java
@@ -115,7 +115,7 @@ abstract public class Hero {
         while (count-- > 0) {
             int rand = (int) (Math.random() * lim);
             out.add(minions.get(temp.get(rand)).clone());
-            temp.set(rand,lim);
+            temp.set(rand,temp.get(lim));
             lim--;
         }
         return out;
