3e650a1f9c4f7f83bf461a45e208fdd2dd6b337c
Mohammed Basioni <mohammedalnamaki@gmail.com>
2020-05-09 23:03:28 +0200

    الكابتن محمد بسيوني يحييكم

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -1,5 +1,26 @@
 package Controller;
 
-public class GameController {
+import engine.GameListener;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameController implements GameListener, ActionListener {
+
+
+
+
+    @Override
+    public void onGameOver() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        new GameController();
+    }
 }
--- src/view/GameView.java
+++ src/view/GameView.java
@@ -1,6 +1,37 @@
 package view;
 
-public class GameView {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class GameView extends JFrame {
+
+    private JPanel herosMenue;
+
+    GameView (){
+//        Full Screen Mood
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setUndecorated(true);
+        setVisible(true);
+//        Exit Button
+        JButton exit = new JButton("Exit");
+        add(exit, BorderLayout.SOUTH);
+        exit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                System.exit(0);
+            }
+        });
+//      Heros menue at the beginning
+
+
+        revalidate();
+        repaint();
+    }
+
+    public static void main(String[] args) {
+        new GameView();
+    }
 
 }
