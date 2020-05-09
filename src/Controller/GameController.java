c807944f3030c3bb8d7dd843767a1b739eec7d63
Mohammed Basioni <mohammedalnamaki@gmail.com>
2020-05-10 00:54:32 +0200

    choosing heros

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -1,14 +1,105 @@
 package Controller;
 
+import engine.Game;
 import engine.GameListener;
+import exceptions.FullHandException;
+import model.heroes.*;
+import view.GameView;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class GameController implements GameListener, ActionListener {
 
+    private Game model;
+    private GameView view;
+    private ArrayList<Hero> Heros;
+    private Hero p1;
+    private Hero p2;
+    private ArrayList<JButton> herosButtons;
+    int c = 0;
+    public GameController () {
+        view = new GameView();
+
+//        adding heros to Jpanel
+        generateHeros();
+
+        view.revalidate();
+        view.repaint();
+    }
+
+    public void generateHeros () {
+        herosButtons = new ArrayList<>();
+        herosButtons.add(new JButton("Hunter"));
+        herosButtons.add(new JButton("Mage"));
+        herosButtons.add(new JButton("Paladin"));
+        herosButtons.add(new JButton("Priest"));
+        herosButtons.add(new JButton("Warlock"));
+        for (int i=0;i<herosButtons.size();i++) {
+            JButton b = herosButtons.get(i);
+            b.addActionListener(this);
+            view.getHerosMenue().add(b);
+        }
+
+    }
+    public Hero switchOnHeros (JButton b) {
+        Hero p = null;
+        switch (b.getActionCommand()) {
+            case "Hunter":
+                try {
+                    p = new Hunter();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } catch (CloneNotSupportedException ex) {
+                    ex.printStackTrace();
+                }
+                break;
+            case "Mage":
+                try {
+                    p = new Mage();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } catch (CloneNotSupportedException ex) {
+                    ex.printStackTrace();
+                }
+                break;
+            case "Paladin":
+                try {
+                    p = new Paladin();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } catch (CloneNotSupportedException ex) {
+                    ex.printStackTrace();
+                }
+                break;
+            case "Priest":
+                try {
+                    p = new Priest();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } catch (CloneNotSupportedException ex) {
+                    ex.printStackTrace();
+                }
+                break;
+            case "Warlock":
+                try {
+                    p = new Warlock();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } catch (CloneNotSupportedException ex) {
+                    ex.printStackTrace();
+                }
+                break;
 
 
+
+        }
+        return p;
+    }
 
     @Override
     public void onGameOver() {
@@ -17,6 +108,32 @@ public class GameController implements GameListener, ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        JButton b = (JButton) e.getSource();
+//        choosing players' heros:
+        Hero p = null;
+        if (!b.getActionCommand().equals("Exit")) {
+            if (c == 0) {
+                p = switchOnHeros(b);
+                c++;
+            }
+        }
+        if (c == 1) {
+            view.getHerosMenueLabel().setText("Second Player: ");
+            p1 = p;
+        }
+        else if (c == 2) {
+            p2 = p;
+            try {
+                model = new Game(p1,p2);
+            } catch (FullHandException ex) {
+                ex.printStackTrace();
+            } catch (CloneNotSupportedException ex) {
+                ex.printStackTrace();
+            }
+            c++;
+        }
+//        ......................
+
 
     }
 
--- src/view/GameView.java
+++ src/view/GameView.java
@@ -1,37 +1,57 @@
 package view;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GameView extends JFrame {
 
-    private JPanel herosMenue;
+    public JPanel getHerosMenue() {
+        return herosMenue;
+    }
 
-    GameView (){
+    private JPanel herosMenue;
+    private JLabel herosMenueLabel = new JLabel("First Player:");
+
+    public JLabel getHerosMenueLabel() {
+        return herosMenueLabel;
+    }
+
+    public GameView(){
 //        Full Screen Mood
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setUndecorated(true);
         setVisible(true);
-//        Exit Button
+        setLayout(new FlowLayout());
+
+//      Heros menue
+        herosMenue = new JPanel();
+        herosMenue.setLayout(new GridLayout(0,1));
+        herosMenue.add(herosMenueLabel);
+        add(herosMenue);
+        herosMenue.setPreferredSize(new Dimension(400,getHeight()-100));
+
+
+        //        Exit Button
         JButton exit = new JButton("Exit");
-        add(exit, BorderLayout.SOUTH);
+
+        add(exit);
         exit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e)
             {
                 System.exit(0);
             }
         });
-//      Heros menue at the beginning
-
-
+        exit.setSize(new Dimension(50,200));
+        exit.setLocation(100,200);
         revalidate();
         repaint();
     }
 
     public static void main(String[] args) {
-        new GameView();
+
     }
 
 }
