ba45d30116094eaf36d268deb33e6cc771c739c8
Mohammed Basioni <mohammedalnamaki@gmail.com>
2020-05-13 02:28:23 +0200

    هالو مازر فازر

--- src/exceptions/CannotAttackException.java
+++ src/exceptions/CannotAttackException.java
@@ -1,12 +1,19 @@
 package exceptions;
 
+import Controller.GameController;
+
+import static Controller.GameController.playSound;
+
 public class CannotAttackException extends HearthstoneException {
     public CannotAttackException(){
-        super("You can NOT attack this card.");
+        super("You can NOT attack this card. The minion is sleeping.");
     }
     public CannotAttackException(String s){
-
         super(s);
     }
+
+//    public void playSoundExeption (){
+//        playSound("sounds/Class Heroes/Mage/YouCanNotAttackThisCard.wav");
+//    }
 
 }
